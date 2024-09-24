146c27755b5454862c699d6f94802875d8d14233
arthur <gyulabyana@gmail.com>
2024-09-25 03:52:54 +0400

    create vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,7 +6,42 @@ const store = createStore({
       todos: []
     };
   },
-  mutations: {}
+  mutations: {
+    setTodos(state, todos) {
+      state.todos = todos;
+    },
+    addTodo(state, todo) {
+      state.todos = [todo, ...state.todos];
+    },
+    updateTodo(state, updatedTodo) {
+      const index = state.todos.findIndex((todo) => todo.id === updatedTodo.id);
+      if (index !== -1) {
+        state.todos[index] = updatedTodo;
+      }
+    },
+    deleteTodo(state, todoId) {
+      state.todos = state.todos.filter((todo) => todo.id !== todoId);
+    }
+  },
+  actions: {
+    saveTodosAction({ commit }, todos) {
+      commit('setTodos', todos);
+    },
+    addTodoAction({ commit }, todo) {
+      commit('addTodo', todo);
+    },
+    updateTodoAction({ commit }, updatedTodo) {
+      commit('updateTodo', updatedTodo);
+    },
+    deleteTodoAction({ commit }, todoId) {
+      commit('deleteTodo', todoId);
+    }
+  },
+  getters: {
+    allTodos(state) {
+      return state.todos;
+    }
+  }
 });
 
 export default store;
